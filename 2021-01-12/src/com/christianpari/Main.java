9cc0c4ec7752de27229e25e2d766891a23ca506c
Christian Pari <christianpari@outlook.com>
2021-01-15 17:41:49 -0500

    Fixed issues that occured when the time would be either midnight or midday and also getting incorrect period when standard time was asked for

--- 2021-01-12/src/com/christianpari/Main.java
+++ 2021-01-12/src/com/christianpari/Main.java
@@ -5,16 +5,21 @@ public class Main {
   public static void main(String[] args) {
     Utility util = new Utility();
     // VERSION 1
+    System.out.print("Version 1 --- ");
     util.convertHoursV1(21, 0, 36);
     // VERSION 2
+    System.out.print("Version 2 --- ");
     util.convertHoursV2(1, 15, -5);
     // VERSION 3
-    util.convertMinsV1(21, 00, 147);
+    System.out.print("Version 3 --- ");
+    util.convertMinsV1(12, 00, 0);
     // VERSION 4
-    util.convertHoursV3(21, 27, 2, true);
-    util.convertMinsV2(11, 0, 147, true);
+    System.out.println("\nVersion 4");
+    util.convertHoursV3(11, 00, 13, false);
+    util.convertMinsV2(11, 0, 59, true);
     // VERSION 5
-    util.convertHoursV4(9, 27, "pm", 12, true);
-    util.convertMinsV3(9, 12, "pm", 147, true);
+    System.out.println("\nVersion 5");
+    util.convertHoursV4(12, 0, "am", 12, true);
+    util.convertMinsV3(11, 0, "pm", 0, true);
   }
 }
--- 2021-01-12/src/com/christianpari/Utility.java
+++ 2021-01-12/src/com/christianpari/Utility.java
@@ -19,7 +19,7 @@ public class Utility {
   ) {
     int newTime = (hours + hoursPassed) % 24;
     if (newTime < 0) {
-      newTime = 24 + newTime;
+      newTime += 24;
     }
     System.out.println(newTime + ":" + (mins < 10 ? "0" + mins : mins));
   }
@@ -36,7 +36,6 @@ public class Utility {
       negativeMins = true;
     }
     int addedHours = minsPassed / 60;
-//    minsPassed -= (60 * addedHours);
     minsPassed = minsPassed % 60;
     mins = negativeMins ? mins - minsPassed : mins + minsPassed;
     if (mins > 59) {
@@ -49,7 +48,7 @@ public class Utility {
     }
     int newHours = negativeMins ? (hours - addedHours) % 24 : (hours + addedHours) % 24;
     if (newHours < 0) {
-      newHours = 24 + newHours;
+      newHours += 24;
     }
     String newTime = newHours + ":" + (mins < 10 ? "0" + mins : mins);
     System.out.println(newTime);
@@ -64,12 +63,13 @@ public class Utility {
   ) {
     int newTime = (hours + hoursPassed) % 24;
     if (newTime < 0) {
-      newTime = 24 + newTime;
+      newTime += 24;
     }
+    String period = newTime > 11 ? " PM" : " AM";
     if (standard) {
-      newTime = newTime % 12;
+      newTime = (newTime % 12 != 0) ? newTime % 12 : 12;
     }
-    System.out.println(newTime + ":" + (mins < 10 ? "0" + mins : mins));
+    System.out.println(newTime + ":" + (mins < 10 ? "0" + mins : mins) + (standard ? period : ""));
   }
 
   public void convertMinsV2(
@@ -84,7 +84,6 @@ public class Utility {
       negativeMins = true;
     }
     int addedHours = minsPassed / 60;
-//    minsPassed -= (60 * addedHours);
     minsPassed = minsPassed % 60;
     mins = negativeMins ? mins - minsPassed : mins + minsPassed;
     if (mins > 59) {
@@ -99,8 +98,8 @@ public class Utility {
     if (adjustedHours < 0) {
       adjustedHours = 24 + adjustedHours;
     }
-    int newHours = standard ? adjustedHours % 12 : adjustedHours;
-    String newTime = newHours + ":" + (mins < 10 ? "0" + mins : mins) + (standard ? (adjustedHours > 12 ? " PM" : " AM") : "");
+    int newHours = standard ? (adjustedHours % 12 == 0 ? 12 : adjustedHours % 12) : adjustedHours;
+    String newTime = newHours + ":" + (mins < 10 ? "0" + mins : mins) + (standard ? (adjustedHours > 11 ? " PM" : " AM") : "");
     System.out.println(newTime);
   }
 
@@ -112,16 +111,17 @@ public class Utility {
     int hoursPassed,
     boolean standard
   ) {
-    if (suffix.equals("PM") || suffix.equals("Pm") || suffix.equals("pm")) {
+    boolean isPM = suffix.equalsIgnoreCase("pm");
+    if ((isPM && hours != 12) || (!isPM && hours == 12)) {
       hours += 12;
     }
     int newTime = (hours + hoursPassed) % 24;
-    String newSuffix = newTime > 12 ? "PM" : "AM";
+    String newSuffix = newTime > 11 ? "PM" : "AM";
     if (newTime < 0) {
-      newTime = 24 + newTime;
+      newTime += 24;
     }
     if (standard) {
-      newTime = newTime % 12;
+      newTime = (newTime % 12 != 0) ? newTime % 12 : 12;
     }
     System.out.println(newTime + ":" + (mins < 10 ? "0" + mins : mins) + " " + (standard ? newSuffix : ""));
   }
@@ -133,32 +133,42 @@ public class Utility {
     int minsPassed,
     boolean standard
   ) {
-    if (suffix.equals("PM") || suffix.equals("Pm") || suffix.equals("pm")) {
+    // checking suffix first to adjust hours accordingly
+    boolean isPM = suffix.equalsIgnoreCase("pm");
+    if ((isPM && hours != 12) || (!isPM && hours == 12)) {
       hours += 12;
     }
-    boolean negativeMins = false;
-    if (minsPassed < 0) {
-      minsPassed -= (minsPassed * 2);
+    boolean negativeMins = false; // boolean check to handle converting of mins later
+    if (minsPassed < 0) { // negative mins were passed
+      minsPassed -= (minsPassed * 2); // convert minsPassed to a positive number
       negativeMins = true;
     }
-    int addedHours = minsPassed / 60;
-//    minsPassed -= (60 * addedHours);
-    minsPassed = minsPassed % 60;
-    mins = negativeMins ? mins - minsPassed : mins + minsPassed;
+    int addedHours = minsPassed / 60; // extract amount of hours from total mins
+    minsPassed = minsPassed % 60; // assigns the leftover mins to minsPassed
+    mins = negativeMins ? mins - minsPassed : mins + minsPassed; // negativeMins = true -> subtract minsPassed - mins [else] add them
     if (mins > 59) {
-      addedHours += 1;
-      mins -= 60;
+      addedHours += 1; // add another hour since mins became greater than 59
+      mins -= 60; // subtract them from mins
     }
-    if (mins < 0) {
-      mins = 60 + mins;
-      hours -= 1;
+    if (mins < 0) { // mins are negative?
+      mins += 60; // make mins positive by adding 60 (which actually is 60 - mins since mins is negative)
+      hours -= 1; // subtract 1 from total hours
     }
+
+    // adjusted hours are handling whether the addedHours are being added or subtracted
     int adjustedHours = negativeMins ? (hours - addedHours) % 24 : (hours + addedHours) % 24;
-    if (adjustedHours < 0) {
-      adjustedHours = 24 + adjustedHours;
+    if (adjustedHours < 0) { // negative hours adjust to correct positive hours
+      adjustedHours += 24;
     }
-    int newHours = standard ? adjustedHours % 12 : adjustedHours;
-    String newTime = newHours + ":" + (mins < 10 ? "0" + mins : mins) + (standard ? (adjustedHours > 12 ? " PM" : " AM") : "");
+
+    // if standard time convert else leave as military
+    int newHours = standard ? (adjustedHours % 12 == 0 ? 12 : adjustedHours % 12) : adjustedHours;
+
+    String minString = ":" + (mins < 10 ? "0" + mins : mins); // include a 0 with mins or not
+
+    // concat the correct period am or pm if standard is true, otherwise concat an empty string
+    String period = (standard ? (adjustedHours <= 11 ? " AM" : " PM") : "");
+    String newTime = newHours + minString + period;
     System.out.println(newTime);
   }
 }
